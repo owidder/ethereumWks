bf4fdef2e18bc35600a13c27a9082742bee9f0c2
Oliver Widder <oliver.widder@gmail.com>
2018-04-09 07:58:29 +0200

    read event data

--- js/chainTrazeEthers.js
+++ js/chainTrazeEthers.js
@@ -5,13 +5,21 @@ const provider = new ethers.providers.Web3Provider(web3Provider);
 
 const chainTrazeContractInfo = require('./chainTrazeContractInfo');
 
+const chainTrazeInterface = new ethers.Interface(chainTrazeContractInfo.abi);
+console.log(chainTrazeInterface);
+
 async function callWithEthers() {
     const accounts = await provider.listAccounts();
     console.log(accounts);
 
     const contract = new ethers.Contract(chainTrazeContractInfo.address, chainTrazeContractInfo.abi, provider);
-    const result = await contract.getPositionContent(0, 0);
+    const result = await contract.getPositionContent(55, 66);
     console.log(result);
+
+    provider.on(["0x5dfb2295f6c23ae130d55b1106b02184d4a217886880ae1d0f37123542574775"], function (log) {
+        console.log(log);
+        console.log(chainTrazeInterface.events.Position2.parse(log.data));
+    })
 }
 
 callWithEthers();
